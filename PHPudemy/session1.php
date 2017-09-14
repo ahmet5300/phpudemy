e18d0e9bf000db27c1698ae3d87f45b92c2792cf
Ahmet5300 <25asahin25@gmail.com>
2017-09-14 10:35:49 +0200

    PHP: sessions
    Als developer wil ik zien hoe sessions werken, zodat ik het kan
    gebruiken indien het nodig is.

---
+++ PHPudemy/session1.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+$_SESSION["tutorials"]="Ahmet is watching php tutorials. ";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	
+</body>
+</html>
