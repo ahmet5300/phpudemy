5b3beee4649e870cadad46a2f5d84a3df82884aa
Ahmet5300 <25asahin25@gmail.com>
2017-09-14 09:58:10 +0200

    PHP: reading cookies
    Als developer wil ik de cookies kunnen aanpassen, zodat ik de website
    gebruiksvriendelijker kan maken.

--- PHPudemy/cookies.php
+++ PHPudemy/cookies.php
@@ -1,7 +1,7 @@
 <?php
 $name="Ahmet";
-$value=15000000;
-$expiration= time() + (60*60*24*8);
+$value=1400000;
+$expiration= time() + (60*60*24*7);
 
 setcookie($name,$value,$expiration);
 
@@ -15,6 +15,16 @@ setcookie($name,$value,$expiration);
 	<title>Document</title>
 </head>
 <body>
+<?php
+
+if(isset($_COOKIE["Ahmet"])){
+$me= $_COOKIE["Ahmet"];
+echo $me;
+}else{
+	$me="";
+}
+	
+?>
 	
 </body>
 </html>
