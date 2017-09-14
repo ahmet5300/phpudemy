51a97d6eee4293e3ee7e81dae9ff50e670919719
Ahmet5300 <25asahin25@gmail.com>
2017-09-14 16:45:15 +0200

    PHP: data access
    Als developer wil ik verschil tussen private/public/protected weten,
    zodat ik weet welke ik moet gebruiken in verschillende situaties.

---
+++ PHPudemy/car_access.php
@@ -0,0 +1,20 @@
+<?php
+
+class Car{
+	public $wheel=4;
+	protected $engine=1;
+	private $hood=1;
+	var $doors=4;
+	
+function __construct(){
+	echo $this->hood=4;
+}
+}
+
+$gleamg = new Car();
+$m6= new Car();
+
+?>
+
+
+
