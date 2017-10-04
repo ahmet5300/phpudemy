227542e45ef3789f1f7547314cddb749072f746c
ahmet5300 <25ahmet@live.nl>
2017-10-04 16:19:20 +0200

    PHP:  adding categories
    Als developer wil ik  een categorie kunnen plaatsen op de admin panel,
    zodat ik alles makkelijk kan wijzigen.

--- PHPudemy/cms/admin/categories.php
+++ PHPudemy/cms/admin/categories.php
@@ -14,11 +14,31 @@
                        
                            
                              <h1 class="page-header">
-                            Blank Page
-                            <small>AHmet</small>
+                            Category
+                            <small>ADD</small>
                         </h1>
                         <div class="col-xs-6">
-                        <form action="">
+                        
+								 <?php
+								if(isset($_POST['submit'])){
+									$cat_title=$_POST['cat_title'];
+									if($cat_title == " " || empty($cat_title)){
+										echo "It doesn't work with a empty box!";
+									}
+									else {
+										$query= "INSERT INTO categories(cat_title) ";
+										$query .= "VALUE('{$cat_title}')";
+
+										$result_categories= mysqli_query($connection, $query);
+
+										if(!$result_categories){
+											die("Sorry failure please try later" . myswli_error($connection));
+										}
+									}
+								}?>
+         
+             
+                        <form action="" method="post">
                         	<div class="form-group">
                         		<label for="cat-title">Add Category:</label>
                         		<input type="text" name="cat_title" class="form-control">
@@ -26,14 +46,15 @@
                         	<div class="form-group">
                         		<input type="submit" name="submit" value="Submit" class="btn btn-primary">
                         	</div>
+                        	
                       </form></div>
                         
   					 <div class="col-xs-6">
-   	<?php
-	$query= "SELECT * FROM categories";
-	$result= mysqli_query($connection,$query);
-	
-	?>
+							<?php
+							$query= "SELECT * FROM categories";
+							$result= mysqli_query($connection,$query);
+
+							?>
                        	<table class="table table-bordered table-hover">
                        		<thead>
                        			<tr>
@@ -42,17 +63,17 @@
                        			</tr>
                        		</thead>
                        		<tbody>
-     <?php
-	while ($row=mysqli_fetch_assoc($result))
-	{
-		$cat_id=$row['cat_id'];
-		$cat_title=$row['cat_title'];
-		echo "<tr>";
-		echo "<td>{$cat_id}</td>";
-		echo "<td>{$cat_title}</td>";
-		echo "</tr>";
-	}
-	?>
+								 <?php
+								while ($row=mysqli_fetch_assoc($result))
+								{
+									$cat_id=$row['cat_id'];
+									$cat_title=$row['cat_title'];
+									echo "<tr>";
+									echo "<td>{$cat_id}</td>";
+									echo "<td>{$cat_title}</td>";
+									echo "</tr>";
+								}
+								?>
                        			
                        		</tbody>
                        	</table>
